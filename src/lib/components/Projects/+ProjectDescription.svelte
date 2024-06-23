4ef6b41260d8b6a18daada2b9a1c772afb45e752
Jason Maritz <jasonmaritz321@gmail.com>
2024-06-23 15:43:33 +0200

    basic project creation frontend

---
+++ src/lib/components/Projects/+ProjectDescription.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import { Carta, MarkdownEditor, Markdown } from 'carta-md';
+	// Component default theme
+	import '$lib/styles/github.scss';
+
+	const carta = new Carta();
+	let value = '';
+
+	//methods
+	function createProject(){
+
+	}
+</script>
+
+<div class="flex flex-col space-y-10 w-9/12">
+	<div class="space-y-2">
+		<h2>Short Description</h2>
+		<input class="input" type="text" title="Short Description" placeholder="project name..." />
+	</div>
+	<div class="space-y-2 h-4/6">
+		<h2>Long Description</h2>
+		<MarkdownEditor bind:value {carta} mode="tabs" theme="github" />
+	</div>
+	<div class="flex justify-end space-x-3">
+		<button type="button" class="btn variant-glass-secondary">cancel</button>
+		<button type="button" class="btn variant-filled-primary" on:click={createProject}>Save Project</button>
+	</div>
+</div>
+
+<style>
+	/* Or in global stylesheet */
+	/* Set your custom monospace font */
+	:global(.carta-font-code) {
+		font-family: '...', monospace;
+		font-size: 1.1rem;
+	}
+</style>
--- src/routes/project/new/+page.svelte
+++ src/routes/project/new/+page.svelte
@@ -1,8 +1,7 @@
-<div class="container h-full mx-auto flex flex-row justify-center items-center"> 
-	<div class="flex justify-center flex-col space-y-5">
-		<h1 class="h1">This page is very empty...</h1>
-		<p>
-			Give us some time to clean out some cobwebs, please check out other pages in the mean time
-		</p>
-	</div>
+<script>
+	import ProjectDescription from '$lib/components/Projects/+ProjectDescription.svelte';
+</script>
+
+<div class="container h-full mx-auto flex justify-center">
+	<ProjectDescription />
 </div>
